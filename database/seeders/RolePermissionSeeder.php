4c6b1e13b7fc7705b954ac7179f5b7c29f5f80f8
Angga Kusuma <angga.codebros@gmail.com>
2025-10-09 22:39:54 +0800

    Chore: Breeze Spatie Seeder

---
+++ database/seeders/RolePermissionSeeder.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+
+class RolePermissionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+        $permissions = [
+            'manage statistics',
+            'manage products',
+            'manage principles',
+            'manage testimonials',
+            'manage clients',
+            'manage teams',
+            'manage abouts',
+            'manage appointments',
+            'manage hero section',
+        ];
+
+        foreach ($permissions as $permission) {
+            Permission::firstOrCreate(
+                [
+                    'name' => $permission,
+                ]
+            );
+        }
+
+        $designManagerRole = Role::firstOrCreate([
+            'name' => 'designer_manager',
+        ]);
+
+        $designManagerPermission = [
+            'manage products',
+            'manage principles',
+            'manage testimonials',
+        ];
+
+        $designManagerRole->syncPermissions($designManagerPermission);
+
+        $superAdminRole = Role::firstOrCreate([
+            'name' => 'super_admin',
+        ]);
+
+        $user = User::create([
+            'name' => 'Magana',
+            'email' => 'contact.genyol@gmail.com',
+            'password' => bcrypt('secret'),
+        ]);
+
+        $user->assignRole($superAdminRole);
+    }
+}
