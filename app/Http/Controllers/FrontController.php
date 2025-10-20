6aa422483b5d4a70e8d7ac80d6b9922a7e676448
Angga Kusuma <angga.codebros@gmail.com>
2025-10-20 14:18:52 +0800

    feat: get home page

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -2,10 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\CompanyStatistic;
+use App\Models\HeroSection;
+use App\Models\OurPrinciple;
+use App\Models\OurTeam;
+use App\Models\Product;
+use App\Models\Testimonial;
+
 class FrontController extends Controller
 {
     public function index()
     {
-        return view('front.index');
+        $statistics = CompanyStatistic::latest('id')->take(4)->get();
+        $principles = OurPrinciple::latest('id')->take(3)->get();
+        $products = Product::latest('id')->take(3)->get();
+        $teams = OurTeam::latest('id')->take(7)->get();
+        $testimonials = Testimonial::latest('id')->take(3)->get();
+        $hero_sections = HeroSection::orderByDesc('id')->take(1)->get();
+
+        return view('front.index', compact('statistics', 'principles', 'products', 'teams', 'testimonials', 'hero_sections'));
     }
 }
